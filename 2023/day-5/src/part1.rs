93ecf31418d3afcdf7f17bebb7dff119934617bc
Deepak Kumar <deepak.kumar.ext@gmail.com>
2023-12-12 21:16:59 +0100

    chore: wip day-5 part-1

--- 2023/day-5/src/part1.rs
+++ 2023/day-5/src/part1.rs
@@ -1,4 +1,46 @@
-pub fn solve(input: &str) -> u32 {
+use std::collections::HashMap;
+
+type MyMap = Vec<Data>;
+
+struct Data {
+    dest: u64,
+    src: u64,
+    length: u64,
+}
+
+fn get(my_map: &MyMap, src_key: u64) -> Option<u64> {
+    for data in my_map {
+        let diff = src_key - data.src;
+        if src_key < data.src && diff < data.length {
+            return Some(data.dest + diff);
+        }
+    }
+    None
+}
+
+pub fn solve(input: &str) -> u64 {
+    let seed_to_soil: MyMap = Vec::new();
+    let soil_to_fertilizer: MyMap = Vec::new();
+    let fertilizer_to_water: MyMap = Vec::new();
+    let water_to_light: MyMap = Vec::new();
+    let light_to_temp: MyMap = Vec::new();
+    let temp_to_humidity: MyMap = Vec::new();
+    let humidity_to_location: MyMap = Vec::new();
+    let mut seeds: Vec<u64> = Vec::new();
+
+    for line in input.lines() {
+        if line.starts_with("seeds:") {
+            seeds = line[6..]
+            .split_whitespace()
+            .map(|d| d.parse::<u64>().unwrap_or_default())
+            .collect();
+        }
+
+        if line.starts_with("seed-to-soil map:") {
+
+        }
+    }
+
     0
 }
 
